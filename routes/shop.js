fd2986579ca09ad13bc6f1c83facf1f9b85f191e
Ritwick Bhargav <ritwickbhargav80@gmail.com>
2020-04-20 00:17:19 +0530

    modifications!

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-let { register, verifyContact, retryContactVerification, addProducts, viewOneProduct, viewProducts, readQrData, qrStatus, addToCart, viewCart, removeFromCart } = require("../controllers/shop_controller");
+let { register, verifyContact, retryContactVerification, addProducts, viewOneProduct, viewProducts, readQrData, customerCount, qrStatus, addToCart, viewCart, removeFromCart } = require("../controllers/shop_controller");
 
 let { adminAuth, staffAuth, customerAuth, allAuth, someAuth, staffandadminAuth } = require("../config/auth");
 
@@ -12,6 +12,7 @@ router.post("/add", staffandadminAuth, addProducts);
 router.get("/viewone/:id", allAuth, viewOneProduct);
 router.get("/viewall", allAuth, viewProducts);
 router.get("/readQrCode/:_id", readQrData);
+router.get("/count", staffandadminAuth, customerCount);
 router.get("/qrStatus", customerAuth, qrStatus);
 router.post("/cart/:id", customerAuth, addToCart);
 router.get("/viewcart", customerAuth, viewCart);
